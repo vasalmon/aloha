063ba583d6796b4756579be97d82648d8b60513f
Victor Salmon <vasalmon@hotmail.com>
2017-04-21 15:52:51 -0700

    Added Libraries and nav IDs

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 console.log("Hello World!");
 console.log($('.nav'));
+
+//Smooth Scrolling attempt
+$(function() {
+  $('nav a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
